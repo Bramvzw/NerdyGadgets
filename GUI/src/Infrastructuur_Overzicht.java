142ccdde7c87e8336a03a2180de7da26e52f9bc8
zoz <yazan111.yy@gmail.com>
2020-05-27 08:32:54 +0200

    resize gui linen

--- GUI/src/Infrastructuur_Overzicht.java
+++ GUI/src/Infrastructuur_Overzicht.java
@@ -5,18 +5,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-
-
-
 public class Infrastructuur_Overzicht extends JPanel {
-
-
-
-
-
     private ArrayList<Componenten> database = new ArrayList<>();
-
-
 
     public Infrastructuur_Overzicht(ArrayList<Componenten> component , Applicatie app)  {
         setBackground(Color.lightGray);
@@ -24,21 +14,15 @@ public class Infrastructuur_Overzicht extends JPanel {
         System.out.println(database.size());
         System.out.println();
 
-
-
     }
     public void setcomponenten(ArrayList<Componenten> a){
         this.database= a;
         System.out.println(database.size());
-
-  repaint();
+     repaint();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-
-
 
         int xDatabase = 50;
        int xWebserver = 400;
@@ -56,7 +40,7 @@ public class Infrastructuur_Overzicht extends JPanel {
             try {
                 Image image = ImageIO.read(new File("Images/Database.png")).getScaledInstance(50, 50, Image.SCALE_SMOOTH);
                 if (s.getType().equals("DBserver")) { // draw database
-                    if (dCount == 4 || dCount == 8) {
+                    if (dCount == 7 || dCount == 14) {
                         xDatabase += 100;
                         yDatabase = 50;
 
@@ -75,7 +59,7 @@ public class Infrastructuur_Overzicht extends JPanel {
 
 
                 } else if (s.getType().equals("webserver")) {
-                    if (wCount == 4 || wCount == 8 ) {
+                    if (wCount == 7 || wCount == 14 ) {
                         xWebserver += 100;
                         yWebserver = 50;
 
